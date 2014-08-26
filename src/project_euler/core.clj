ccc1828cc057e6d347c74e8409dd9c2d45da6f6a
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-08-26 23:05:18 +0200

    Solve #1 and #2

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,6 +1,17 @@
 (ns project-euler.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn divisible? [n d]
+  (zero? (rem n d)))
+
+(defn problem-1 []
+  (apply + (apply clojure.set/union (map #(set (range 0 1000 %)) [3 5]))))
+(defn problem-1 []
+  (apply + (filter #(or (divisible? % 3) (divisible? % 5)) (range 1000))))
+(defn problem-1 []
+  (apply + (distinct (concat (range 0 1000 3) (range 0 1000 5)))))
+
+(defn fib [a b]
+  (cons a (lazy-seq (fib b (+ a b)))))
+
+(defn problem-2 []
+  (apply + (for [n (filter even? (fib 1 2)) :while (< n 4000000)] n)))
