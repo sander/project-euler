4a3bc77372c52638b7f55973e3def1c2003f7987
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2014-08-26 22:28:51 +0200

    Fix Leiningen config

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject project-euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :description "Solutions to Project Euler problems"
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]])
